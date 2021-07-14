0e940c9e579aac6edf168096e291447d8dda7928
dhardestylewis <danielhardestylewis@utexas.edu>
2021-07-14 16:45:27 -0400

    Stylistic changes

--- app.py
+++ app.py
@@ -336,28 +336,6 @@ def show_box(hide_n_clicks, image_n_clicks):
     else:
         return 'Show the Input Form'
 
-#@app.callback(
-#    Output('selected_image','children'),
-#    Input({'type':'select_button','index': ALL}, 'n_clicks'),
-#    State({'type':'select_button','index': ALL}, 'id')
-#)
-#def show_box(n_clicks, entry_id):
-#    # get index of clicked image
-#    triggered = dash.callback_context.triggered[0]['prop_id'].replace('.n_clicks','')
-#    trigger = json.loads(triggered)
-#    trigger_index = trigger['index']
-#    # get image url from image
-#    image_url = image_search.at[trigger_index,'Image_url']
-#    kids = html.Div(
-#        html.Img(
-#            src = image_url,
-#            style = {
-#                'width' : '60vw'
-#            }
-#        )
-#    )
-#    return kids #"{}".format(image_url)
-
 @app.callback(
     Output('selected_image','children'),
     Input({'type':'image-card','index': ALL}, 'n_clicks'),
@@ -467,11 +445,11 @@ def display_page(
 ):
     """Show modal for adding a label."""
     triggered = dash.callback_context.triggered[0]['prop_id'].replace('.n_clicks','')
-    if triggered == "url":
-        return True
+#    if triggered == "url":
+#        return True
     if triggered == "liveview_modal_ok_button_initial":
         return False
-    return False
+    return True
 
 # ----------------------------------------------------------------------------
 # RUN APPLICATION
